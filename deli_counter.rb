9e5cc7ea1ba022725ff39676901c7bed4654be47
Sav Younts <savyounts@gmail.com>
2018-08-03 18:46:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,11 @@ def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{katz_deli["
+    puts "The line is currently:" 
   end 
 end    
+
+def take_a_number (katz_deli, name)
+  place = katz_deli.size + 1
+  puts "Welcome, #{name}. You are number #{place} in line."
+end
